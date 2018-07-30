ecae702b84e4157e9f6d7f5bfb4eb4d1480fcc88
AronWu <dev@inesv.com>
2018-07-30 23:03:05 +0800

    init repo

---
+++ src/main/java/com/inesv/ServerBootApp.java
@@ -0,0 +1,19 @@
+package com.inesv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SuddenAttack Server
+ *
+ * @author SKINK
+ */
+@SpringBootApplication
+public class ServerBootApp {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ServerBootApp.class, args);
+  }
+
+
+}
